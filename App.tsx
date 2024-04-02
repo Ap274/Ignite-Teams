3cc1c3dc800b6fb465f79996cc85213062f4d3df
Ap274 <aldopiresaps@gmail.com>
2024-04-02 18:23:14 -0300

    feat: create structure of component Header

--- App.tsx
+++ App.tsx
@@ -22,7 +22,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { !fontsLoaded ? <Groups /> : <Loading />}
+      { fontsLoaded ? <Groups /> : <Loading />}
 
     </ThemeProvider>
   );
