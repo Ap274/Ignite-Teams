3554a23f355a98f8f5d218a883138b1290ed844e
Ap274 <aldopiresaps@gmail.com>
2024-04-04 10:59:24 -0300

    feat: implement and define route files to be rendered

--- App.tsx
+++ App.tsx
@@ -1,16 +1,12 @@
 import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
-import { 
-  useFonts,  
-  Roboto_400Regular,
-  Roboto_700Bold
-} from '@expo-google-fonts/roboto'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 import { Loading } from '@components/Loading';
 
 import theme from './src/theme'
 
-import { Players } from '@screens/Players';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -22,7 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Players /> : <Loading />}
+      { fontsLoaded ? <Routes /> : <Loading />}
 
     </ThemeProvider>
   );
