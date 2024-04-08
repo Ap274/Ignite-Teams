0e77c9bfedae1c67e965a36d0307228b78901812
Ap274 <aldopiresaps@gmail.com>
2024-04-08 15:24:07 -0300

    feat: pass parameters between routes and how to retrieve these parameters using the useRoute hook

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { Container, Content, Icon } from "./styles";
 
 import { Header } from "@components/Header";
@@ -7,10 +9,12 @@ import { Button } from "@components/Button";
 import { useNavigation } from "@react-navigation/native";
 
 export function NewGroup() {
+    const [group, setGroup] = useState('');
+
     const navigation = useNavigation();
 
     function handleNew() { 
-        navigation.navigate('players', { group: 'Rocket'});
+        navigation.navigate('players', { group: group });
     }
 
     return (
@@ -26,6 +30,7 @@ export function NewGroup() {
 
                 <Input 
                     placeholder="Nome da turma"
+                    onChangeText={setGroup}
                 />
 
                 <Button 
