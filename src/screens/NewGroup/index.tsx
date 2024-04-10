780fba00bf1f4be66e38d82d739e0dd267de67e9
Ap274 <aldopiresaps@gmail.com>
2024-04-10 18:15:46 -0300

    feat: deal with errors that occur in the application and display in the best way for the user through alerts in the application

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { Alert } from "react-native";
 
+import { AppError } from "@utils/AppError";
 import { groupCreate } from "@storage/group/groupCreate";
+
 
 import { Container, Content, Icon } from "./styles";
 
@@ -8,7 +12,6 @@ import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
-import { useNavigation } from "@react-navigation/native";
 
 export function NewGroup() {
     const [group, setGroup] = useState('');
@@ -17,11 +20,20 @@ export function NewGroup() {
 
     async function handleNew() {
         try {
+            if(group.trim().length === 0) {
+                return Alert.alert('Novo Grupo', 'Informe o nome da turma.');
+            }
+            
             await groupCreate(group);
             navigation.navigate('players', { group: group });
                 
         } catch (error) {
-          console.log(error);
+            if(error instanceof AppError) {
+                Alert.alert('Novo Grupo', error.message);
+            } else {
+                Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo');
+                console.log(error);
+            }
         }
     }
 
